255317dbdd9f24071e9147450c78b8a25c8ede78
pradip-tcs <100670828+pradip9821-tcs@users.noreply.github.com>
2022-07-28 15:53:42 +0530

    Add more code

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,44 +2,65 @@ pragma solidity ^0.4.17;
 
 contract Lottery {
 
-    address public manager;
-    address[] private players;
+    address     public  manager ;
+    address[]   private players ;
 
     function Lottery() public {
-        manager = msg.sender;
+        manager = msg.sender ;
     }
 
     function Enter() public payable {
-        require(msg.sender != manager);
-        require(players.length < 3);
-        require(msg.value > 0.01 ether ); 
-        players.push(msg.sender);
+        require(
+            msg.sender  != manager 
+        ) ;
+        require(
+            players.length  < 3
+        ) ;
+        require(
+            msg.value   > 0.01 ether 
+        ) ;
+
+        players.push(msg.sender) ;
     }
 
     function rendom() private view returns(uint) {
-        return uint(keccak256(block.difficulty, now, players));
+        return uint(
+            keccak256(
+                block.difficulty,
+                now, 
+                players
+            )
+        ) ;
     }
 
     function pickWinner() public Authorization {
         
-        uint index  = rendom() % players.length;
+        uint index  = rendom() % players.length ;
 
-        players[index].transfer(this.balance - (this.balance * 5 / 100 ));
-        manager.transfer(this.balance);
+        players[index].transfer(
+            this.balance - (
+                this.balance * 5 / 100
+            )
+        ) ;
+        manager.transfer(
+            this.balance
+        ) ;
 
-        players     = new address[](0);
+        players = new address[](
+            0
+        ) ;
     }
 
     function getPlayers() public view returns(address[]) {
-        return players;
+        return players ;
     }
 
     function getPlayerCount() public view returns(uint) {
-        return players.length;
+        return players.length ;
     }
 
     modifier Authorization() {
-        require(msg.sender == manager );
-        _;
+        require(msg.sender == manager ) ;
+        _ ;
     }
 }
